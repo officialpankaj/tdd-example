b8208d6b0c076eb8f05ec9619a56815819f26c65
Pankaj Barman <pankajbarman000@gmail.com>
2025-07-15 16:07:03 +0530

    test: supports newline as delimiter

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -14,4 +14,7 @@ describe("StringCalculator", () => {
   it("returns the sum of multiple comma-separated numbers", () => {
     expect(add("1,2,3,4")).toBe(10);
   });
+  it("supports newline as a delimiter along with comma", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
 });
