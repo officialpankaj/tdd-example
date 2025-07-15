6b62841e2e6e12c30fb9ea92348d868edce0046a
Pankaj Barman <pankajbarman000@gmail.com>
2025-07-15 15:53:03 +0530

    test: returns sum of two comma-separated numbers

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -8,4 +8,7 @@ describe('StringCalculator', () => {
   it('returns the number itself when only one number is provided', () => {
     expect(add("5")).toBe(5);
   });
+   it('returns the sum of two comma-separated numbers', () => {
+    expect(add("1,2")).toBe(3);
+  });
 });
