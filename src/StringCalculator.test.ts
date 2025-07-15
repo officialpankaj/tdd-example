52addb54f6d436337ed649f901d4dc1917438762
Pankaj Barman <pankajbarman000@gmail.com>
2025-07-15 15:31:43 +0530

    test: returns single number as integer

--- src/StringCalculator.test.ts
+++ src/StringCalculator.test.ts
@@ -5,4 +5,7 @@ describe('StringCalculator', () => {
   it('returns 0 for empty string', () => {
     expect(add("")).toBe(0);
   });
+  it('returns the number itself when only one number is provided', () => {
+    expect(add("5")).toBe(5);
+  });
 });
